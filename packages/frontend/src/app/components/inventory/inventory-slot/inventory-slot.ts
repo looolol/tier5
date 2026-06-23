833c7f5dcd9db898902030156b1919852435810d
Robbie Rankin <robbierankin15@gmail.com>
2026-06-23 06:22:57 -0400

    drag and drop

--- packages/frontend/src/app/components/inventory/inventory-slot/inventory-slot.ts
+++ packages/frontend/src/app/components/inventory/inventory-slot/inventory-slot.ts
@@ -1,8 +1,8 @@
-import { Component, computed, Input } from '@angular/core';
+import { Component, computed, EventEmitter, Input, Output } from '@angular/core';
 import { HydratedItem } from '@tier5/bungie-api';
 import { InventoryItem } from '../inventory-item/inventory-item';
 import { CommonModule } from '@angular/common';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragDrop, CdkDropList, DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-inventory-slot',
@@ -17,7 +17,17 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 export class InventorySlot {
   @Input({ required: true }) items: HydratedItem[] = [];
   @Input() isVault = false;
+  @Output() itemDropped = new EventEmitter<CdkDragDrop<any>>();
 
   protected readonly equippedItem = computed(() => this.items.find(i => i.equipped));
   protected readonly inventoryPool = computed(() => this.isVault ? this.items : this.items.filter(i => !i.equipped));
+
+  onDrop(event: CdkDragDrop<any>) {
+    console.log('cdkDragDrop', event);
+    this.itemDropped.emit(event);
+  }
+
+  enterPredicate() {
+    return true;
+  }
 }
--- packages/frontend/src/app/components/inventory/inventory.constants.ts
+++
@@ -1,5 +0,0 @@
-export const GRID_CONFIG = {
-    itemSize: 52,
-    gap: 6,
-    columns: 3,
-} as const;
